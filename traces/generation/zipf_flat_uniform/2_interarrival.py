23425a53ffe41c39d7484ca1f160a734572d9a3a
Sohaib Ahmad <sohaibahmad759@gmail.com>
2023-04-04 21:16:38 -0400

    Flat uniform traces

---
+++ traces/generation/zipf_flat_uniform/2_interarrival.py
@@ -0,0 +1,96 @@
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+SCALE_UP_MULTIPLIER = 1
+
+
+def get_args():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--inpfile', '-i', required=False, default='../common/twitter_04_14_norm.txt',
+                    dest='inpfile', help='Input file to read. Uses the prep-processed output ' +
+                                         'from the INFaaS script')
+    ap.add_argument('--distribution', '-d', required=False, default='distribution.txt',
+                    dest='dist_file', help='Distribution to follow for dividing requests ' +
+                                           'across models')
+    ap.add_argument('--slo', '-s', required=False, default=300, dest='slo',
+                    help='The SLO value to use for all requests (in milliseconds)')
+    ap.add_argument('--out', '-o', required=False, default='../../twitter/asplos/zipf_flat_uniform',
+                    dest='out_path', help='Output path to write the traces to')
+    return ap.parse_args()
+
+
+def main(args):
+    input_file = args.inpfile
+    dist_file = args.dist_file
+    out_path = args.out_path
+    slo = int(args.slo)
+
+    distribution = []
+    num_models = 0
+    with open(dist_file, mode='r') as df:
+        lines = df.readlines()
+        distribution = list(map(lambda x: float(x.rstrip('\n')), lines))
+        num_models = len(distribution)
+        
+    traces = {}
+    for model in range(num_models):
+        traces[model] = []
+
+    trace = []
+    with open(input_file, mode='r') as rf:
+        overall_offset = 0
+
+        requests_per_second = 400
+        seconds = 300
+
+        total_requests = requests_per_second * seconds
+
+        for model in range(num_models):
+            requests_for_model = round(total_requests * distribution[model])
+
+            arrivals = np.ones(requests_for_model)
+            arrivals = (arrivals / sum(arrivals) * seconds * 1000).astype(int)
+
+            print(f'model: {model}, arrivals: {arrivals}, sum: {sum(arrivals)}, '
+                  f'requests for model: {requests_for_model}')
+            
+            current = 0
+            # print(arrivals)
+            uniform_time = -1
+            for time in arrivals:
+                current += time
+                uniform_time = time
+                traces[model].append(current)
+            
+            while current < seconds * 1000:
+                traces[model].append(current)
+                current += uniform_time
+
+    conversions = {}
+    conversion_file = '../common/conversion.txt'
+    with open(conversion_file, mode='r') as rf:
+        lines = rf.readlines()
+        conversions = dict(map(lambda x: (x.split(',')[0], x.split(',')[1].rstrip('\n')), lines))
+
+    out_path = os.path.join(out_path, str(slo))
+    if not(os.path.exists(out_path)):
+        os.makedirs(out_path)
+
+    for model in range(num_models):
+        model_number = str(model + 1)
+        model_name = conversions[model_number]
+        filename = os.path.join(out_path, f'{model_name}.txt')
+        print(filename)
+
+        with open(filename, mode='w') as wf:
+            for arrival_time in sorted(traces[model]):
+                wf.write(f'{int(arrival_time)},1,{slo}\n')
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main(get_args())
